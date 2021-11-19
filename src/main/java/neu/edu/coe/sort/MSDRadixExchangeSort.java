bb730a2a48279e21cd6a2b787ddc40f2a7ac1267
slowpeace2020 <slowpeace2020@gmail.com>
2021-11-19 16:07:35 -0500

    code format

--- src/main/java/neu/edu/coe/sort/MSDRadixExchangeSort.java
+++ src/main/java/neu/edu/coe/sort/MSDRadixExchangeSort.java
@@ -7,35 +7,39 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
-public class MSDRadixExchangeSort{
+public class MSDRadixExchangeSort {
 
-  void   radixexchange (String a [], int l, int r, int bit)
-  {
-
+  void radixexchange(String a[], int l, int r, int bit) {
 
     int i = l;
     int j = r;
 
-    if (r <= l || bit > max_bit) return;
+    if (r <= l || bit > max_bit) {
+      return;
+    }
 
-    while (i!=j) {
-      while (j > i && getBit(a[i], bit) == 0 ) {
+    while (i != j) {
+      while (j > i && getBit(a[i], bit) == 0) {
         i++;
       }
       while (j > i && getBit(a[j], bit) == 1) {
         j--;
       }
-      if(i>=j)break;
-      swap(a, i,j);
+      if (i >= j) {
+        break;
+      }
+      swap(a, i, j);
     }
 
-    if (getBit(a[r], bit) == 0) j++;
-    radixexchange(a, l, j-1, bit+1);
-    radixexchange(a, j, r, bit+1);
+    if (getBit(a[r], bit) == 0) {
+      j++;
+    }
+    radixexchange(a, l, j - 1, bit + 1);
+    radixexchange(a, j, r, bit + 1);
   }
 
 
-  public void swap(String a[],int i,int j){
+  public void swap(String a[], int i, int j) {
     String temp = a[i];
     a[i] = a[j];
     a[j] = temp;
@@ -45,24 +49,22 @@ public class MSDRadixExchangeSort{
   private int max_bit;
 
   /* Iterative method to sort file by exchange radix sort */
-  public void exchange_radixsort(String a[]){
-    for (int i=0;i<a.length;i++){
-      max_bit = Math.max(stringToBit(a[i]).length(),max_bit);
+  public void exchange_radixsort(String a[]) {
+    for (int i = 0; i < a.length; i++) {
+      max_bit = Math.max(stringToBit(a[i]).length(), max_bit);
     }
 
-    radixexchange(a, 0, a.length-1, 0);
+    radixexchange(a, 0, a.length - 1, 0);
   }
 
 
-
-  public String stringToBit(String s){
+  public String stringToBit(String s) {
     s = getPinyin(s);
     byte[] bytes = s.getBytes();
     StringBuilder binary = new StringBuilder();
-    for(byte b:bytes){
+    for (byte b : bytes) {
       int val = b;
-      for (int i = 0; i < 8; i++)
-      {
+      for (int i = 0; i < 8; i++) {
         binary.append((val & 128) == 0 ? 0 : 1);
         val <<= 1;
       }
@@ -70,15 +72,16 @@ public class MSDRadixExchangeSort{
 
     return binary.toString();
   }
-  public int getBit(String s,int pos){
-   String binary = stringToBit(s);
 
-    return binary.length()>pos?Integer.valueOf(binary.charAt(pos)+""):0;
+  public int getBit(String s, int pos) {
+    String binary = stringToBit(s);
+
+    return binary.length() > pos ? Integer.valueOf(binary.charAt(pos) + "") : 0;
   }
 
 
   /* String to pinyin */
-  public String getPinyin(String content){
+  public String getPinyin(String content) {
     char[] chars = content.toCharArray();
     HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
     // 设置大小写
@@ -114,23 +117,19 @@ public class MSDRadixExchangeSort{
   public static void main(String[] args) {
     MSDRadixExchangeSort test = new MSDRadixExchangeSort();
 
-    String[]  a = new String[]{"安","爱","张","公"};
-    for(String s:a){
+    String[] a = new String[]{"安", "爱", "张", "公"};
+    for (String s : a) {
       System.out.println(s);
     }
 
     test.exchange_radixsort(a);
-    for(String s:a){
+    for (String s : a) {
       System.out.println(s);
 
     }
 
 
   }
-
-
-
-
 
 
 }
