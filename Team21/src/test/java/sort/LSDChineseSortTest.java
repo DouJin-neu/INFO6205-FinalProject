0510afedc139393e208ef08dee83adba2493fe14
DouJin-neu <jin.do@northeastern.edu>
2021-11-24 15:37:13 -0500

    Update SortBenchmarkHelper

--- Team21/src/test/java/sort/LSDChineseSortTest.java
+++ Team21/src/test/java/sort/LSDChineseSortTest.java
@@ -1,6 +1,7 @@
 package sort;
 
 
+import benchmark.SortBenchmarkHelper;
 import org.junit.Test;
 import sort.utils.MSDCoderFactory;
 
@@ -38,70 +39,12 @@ public class LSDChineseSortTest {
     @Test
     public void sortFile1() throws IOException {
         int n = 41;
-        String[] words = getWords("shuffledChineseTest.txt", LSDChineseSortTest::lineAsList);
+        SortBenchmarkHelper helper = new SortBenchmarkHelper();
+        String[] words = helper.getWords("shuffledChineseTest.txt", LSDChineseSortTest::lineAsList);
         final LSDChineseSort<String> sorter = new LSDChineseSort<>(MSDCoderFactory.pinyinCoder);
         sorter.sort(words);
         assertEquals("边黎明", words[0]);
         assertEquals("马文春", words[27]);
-    }
-
-    /**
-     * Create a string representing an integer, with commas to separate thousands.
-     *
-     * @param x the integer.
-     * @return a String representing the number with commas.
-     */
-    public static String formatWhole(final int x) {
-        return String.format("%,d", x);
-    }
-
-    /**
-     * Method to open a resource relative to this class and from the corresponding File, get an array of Strings.
-     *
-     * @param resource           the URL of the resource containing the Strings required.
-     * @param stringListFunction a function which takes a String and splits into a List of Strings.
-     * @return an array of Strings.
-     */
-    static String[] getWords(final String resource, final Function<String, List<String>> stringListFunction) {
-        try {
-            final File file = new File(getPathname(resource, DualPivotChineseSortTest.class));
-            final String[] result = getWordArray(file, stringListFunction, 1);
-            System.out.println("getWords: testing with " + formatWhole(result.length) + " unique words: from " + file);
-            return result;
-        } catch (final FileNotFoundException e) {
-            System.out.println("Cannot find resource: " + resource);
-            return new String[0];
-        }
-    }
-
-    private static List<String> getWordList(final FileReader fr, final Function<String, List<String>> stringListFunction, final int minLength) {
-        final List<String> words = new ArrayList<>();
-        for (final Object line : new BufferedReader(fr).lines().toArray())
-            words.addAll(stringListFunction.apply((String) line));
-        return words.stream().distinct().filter(s -> s.length() >= minLength).collect(Collectors.toList());
-    }
-
-    /**
-     * Method to read given file and return a String[] of its content.
-     *
-     * @param file               the file to read.
-     * @param stringListFunction a function which takes a String and splits into a List of Strings.
-     * @param minLength          the minimum acceptable length for a word.
-     * @return an array of Strings.
-     */
-    static String[] getWordArray(final File file, final Function<String, List<String>> stringListFunction, final int minLength) {
-        try (final FileReader fr = new FileReader(file)) {
-            return getWordList(fr, stringListFunction, minLength).toArray(new String[0]);
-        } catch (final IOException e) {
-            System.out.println("Cannot open file: " + file);
-            return new String[0];
-        }
-    }
-
-    private static String getPathname(final String resource, @SuppressWarnings("SameParameterValue") final Class<?> clazz) throws FileNotFoundException {
-        final URL url = clazz.getClassLoader().getResource(resource);
-        if (url != null) return url.getPath();
-        throw new FileNotFoundException(resource + " in " + clazz);
     }
 
     static List<String> lineAsList(final String line) {
