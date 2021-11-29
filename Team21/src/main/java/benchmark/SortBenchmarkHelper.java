75316f2d7726263c528900c9790d92f85943894c
DouJin-neu <jin.do@northeastern.edu>
2021-11-29 10:35:31 -0500

    update ShuffledChinese and create a test for the file

--- Team21/src/main/java/benchmark/SortBenchmarkHelper.java
+++ Team21/src/main/java/benchmark/SortBenchmarkHelper.java
@@ -82,40 +82,5 @@ public class SortBenchmarkHelper {
         throw new FileNotFoundException(resource + " in " + clazz);
     }
 
-    /*// TEST
-    static String[] getWords(String resource, Function<String, Collection<String>> getStrings) throws FileNotFoundException {
-        List<String> words = new ArrayList<>();
-        FileReader fr = new FileReader(getFile(resource, SortBenchmarkHelper.class));
-        for (Object line : new BufferedReader(fr).lines().toArray()) words.addAll(getStrings.apply((String) line));
-        words = words.stream().distinct().filter(new Predicate<String>() {
-            private static final int MINIMUM_LENGTH = 2;
-
-            public boolean test(String s) {
-                return s.length() >= MINIMUM_LENGTH;
-            }
-        }).collect(Collectors.toList());
-        logger.info("Testing with words: " + formatWhole(words.size()) + " from " + resource);
-        String[] result = new String[words.size()];
-        result = words.toArray(result);
-        return result;
-    }
-
-    static Collection<String> getWords(Pattern regex, String line) {
-        final Matcher matcher = regex.matcher(line);
-        if (matcher.find()) {
-            final String word = matcher.group(1);
-            final String[] strings = word.split("[\\s\\p{Punct}\\uFF0C]");
-            return Arrays.asList(strings);
-        } else
-            return new ArrayList<>();
-    }
-
-    // TEST
-    private static String getFile(String resource, @SuppressWarnings("SameParameterValue") Class<?> clazz) throws FileNotFoundException {
-        final URL url = clazz.getClassLoader().getResource(resource);
-        if (url != null) return url.getFile();
-        throw new FileNotFoundException(resource + " in " + clazz);
-    }*/
-
 
 }
